32844724691efc45c8f15e2e149810a8ce27eee5
Doom9527 <1633486422@qq.com>
2024-08-26 14:41:30 +0800

    添加结点关联

--- src/main/java/com/paiondata/aristotle/repository/RelationRepository.java
+++ src/main/java/com/paiondata/aristotle/repository/RelationRepository.java
@@ -1,9 +1,20 @@
 package com.paiondata.aristotle.repository;
 
+import com.paiondata.aristotle.model.entity.GraphNode;
 import com.paiondata.aristotle.model.entity.Relation;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 @Repository
-public interface RelationRepository extends Neo4jRepository<Relation, Long> {
+public interface RelationRepository extends Neo4jRepository<Relation,Long> {
+    @Query("MATCH p=(n:Person)-[r:Relation]->(m:Person) " +
+            "WHERE id(n)={startNode} and id(m)={endNode} and r.relation={relation}" +
+            "RETURN p")
+    List<Relation> findRelation(@Param("startNode") GraphNode startNode,
+                                @Param("endNode") GraphNode endNode,
+                                @Param("relation") String relation);
 }
